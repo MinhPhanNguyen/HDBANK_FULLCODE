fb48f2144eafe8f4fa3b42c7ef72210e9a284850
MinhPhanNguyen <SWminh0918195615@gmail.com>
2024-11-26 11:53:53 +0700

    Init Project

---
+++ src/server.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const app = express();
+
+const hostname = 'localhost'
+const port = 8017
+
+
+app.get('/', function(req, res) {
+    res.send('<h1>Hello World!</h1><hr>')
+})
+
+app.listen(port, hostname, () => {
+    console.log(`http://${ hostname }:${ port }/`)
+  })
